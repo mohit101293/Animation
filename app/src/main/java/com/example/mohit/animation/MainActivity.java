04cf5fc62f9fbbc1ccfaae24bd32595670932f5d
mohit101293 <mhtsrvstv05@gmail.com>
2016-09-07 13:07:23 -0700

    Initial commit

---
+++ app/src/main/java/com/example/mohit/animation/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.mohit.animation;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView img;
+    TextView tv;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        img=(ImageView)findViewById(R.id.imageView);
+        tv=(TextView)findViewById(R.id.t1);
+        ObjectAnimator moveAnim = ObjectAnimator.ofFloat(img, "Y", 200);
+        moveAnim.setDuration(3000);
+        moveAnim.setInterpolator(new BounceInterpolator());
+        moveAnim.start();
+
+        ObjectAnimator scaleAnim = ObjectAnimator.ofFloat(tv, "scaleX", 1.0f, 2.0f);
+        scaleAnim.setDuration(3000);
+        scaleAnim.setRepeatCount(ValueAnimator.INFINITE);
+        scaleAnim.setRepeatMode(ValueAnimator.REVERSE);
+        scaleAnim.start();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
